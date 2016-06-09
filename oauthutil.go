d16eb7b41370e3feda0582a7cf89277bf7b5e9f2
Aaron S. Joyner <aaron@joyner.ws>
2016-06-09 00:33:30 -0400

    Add saveToken and tokenFromFile functions.

---
+++ oauthutil.go
@@ -0,0 +1,29 @@
+package oauthutil
+
+import (
+	"encoding/gob"
+	"log"
+	"os"
+
+	"golang.org/x/oauth2"
+)
+
+func tokenFromFile(file string) (*oauth2.Token, error) {
+	f, err := os.Open(file)
+	if err != nil {
+		return nil, err
+	}
+	t := new(oauth2.Token)
+	err = gob.NewDecoder(f).Decode(t)
+	return t, err
+}
+
+func saveToken(file string, token *oauth2.Token) {
+	f, err := os.Create(file)
+	if err != nil {
+		log.Printf("Warning: failed to cache oauth token: %v", err)
+		return
+	}
+	defer f.Close()
+	gob.NewEncoder(f).Encode(token)
+}
